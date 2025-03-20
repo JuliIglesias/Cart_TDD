86b390aee717460325eb52ca7bb281fb3637803e
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-03-20 15:51:42 -0300

    add remove 1 product

--- src/test/java/Cart.java
+++ src/test/java/Cart.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
 
-  public boolean isEmpty() {
-    return true;
+  private final List<Product> products;
+
+  public Cart(List<Product> products) {
+    this.products = products;
   }
 
+  public Cart() {
+    this.products = new ArrayList<>();
+  }
+
+  public boolean isEmpty() {
+    return products.isEmpty();
+  }
+
+  public void addProduct(Product product) {
+    products.add(product);
+  }
+
+  public void removeProduct(Product product) {
+    products.clear();
+  }
 }
--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CartTest {
 
@@ -16,4 +15,23 @@ public class CartTest {
     Cart cart1 = new Cart();
     assertTrue(cart1.isEmpty());
   }
+
+  @Test
+  void AddProductToCart_003() {
+    Cart cart1 = new Cart();
+    cart1.addProduct(new Product());
+    assertFalse(cart1.isEmpty());
+  }
+
+  @Test
+  void RemoveProductFromCart_004() {
+    Cart cart1 = new Cart();
+    cart1.addProduct(new Product());
+    assertFalse(cart1.isEmpty());
+
+    cart1.removeProduct(new Product());
+    assertTrue(cart1.isEmpty());
+  }
+
+
 }
