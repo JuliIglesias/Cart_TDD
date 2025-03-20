312ced9a77e11d1bf12215e288660eb53bfb41d9
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-03-20 15:44:01 -0300

    empty cart

---
+++ src/test/java/Cart.java
@@ -0,0 +1,7 @@
+public class Cart {
+
+  public boolean isEmpty() {
+    return true;
+  }
+
+}
--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,4 +1,19 @@
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CartTest {
+
+  @Test
+  void CreateCart_001() {
+    Cart cart1 = new Cart();
+    assertNotNull(cart1);
+  }
+
+  @Test
+  void EmptyCart_002() {
+    Cart cart1 = new Cart();
+    assertTrue(cart1.isEmpty());
+  }
 }
